8b16b35b8e083bb2867120094c6e5d673d756186
umeyiratasoy <umeyiratasoyy@gmail.com>
2024-02-09 11:45:55 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager: ISupplierService
+    {
+        private IApplicantService _applicantService;
+
+        public PttManager(IApplicantService applicantService)
+        {
+            this._applicantService = applicantService;
+        }
+
+        public void GiveMask(Person person)
+        {
+            
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + "için maske verilir");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + "için maske verilemedi");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,14 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+Person person1= new Person();
+person1.FirstName = "Ümeyir";
+person1.LastName = "ATASOY";
+person1.NationalIdentity = 123; //fake
+person1.DateOfBirthYear = 2001;
+
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
+
+Console.ReadLine();
